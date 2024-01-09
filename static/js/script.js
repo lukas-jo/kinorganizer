4e15427daeb80bf535c5bc4642f234d305fcf574
lukas-jo <lukas.jordan@pm.me>
2024-01-09 01:25:47 +0100

    feat(frontend): better styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,57 @@
-const tmdbSearch = document.querySelector("#filmSearchBar");
-tmdbSearch.addEventListener(
-  "keydown",
-  (e) =>
-    fetch(`/api/search-tmdb/${tmdbSearch.value}`)
+const filmSearchBar = document.querySelector("#filmSearchBar");
+const filmList = document.querySelector("#filmList");
+const selectedFilm = document.querySelector("#selectedFilm");
+const tmdbId = document.querySelector("#tmdb_id");
+
+filmSearchBar.addEventListener("keyup", listSearchResult);
+
+function listSearchResult(event) {
+  const query = filmSearchBar.value;
+  if (query.length > 2) {
+    fetch(`/api/search-tmdb/${query}`)
       .then((res) => res.json())
       .then((json) => json.map(filmToHtml).reduce((p, c) => p + c))
-      .then((html) => { document.querySelector('#filmList').innerHTML = html} )
-  ,
-);
-
-function filmToHtml(film) {
-  return `<div id="film-${film.tmdb_id}" class="hover:bg-red-100" onclick="setFilmId(${film.tmdb_id})">
-            <img class="" src="https://image.tmdb.org/t/p/w92${film.poster_path}"
-            <span class="">${film.title}</span>
-            <span class="">${film.year}</span>
-          </div>`
+      .then((html) => {
+        filmList.innerHTML = html;
+      });
+  } else {
+    filmList.innerHTML = "";
+  }
 }
 
-function setFilmId(id) {
-  document.querySelector('#tmdb_id').value = id;
-  const html = document.querySelector(`#film-${id}`).outerHTML;
-  document.querySelector('#filmList').innerHTML = '';
-  document.querySelector('#filmSearchBar').value = '';
-  document.querySelector('#selectedFilm').innerHTML = html;
+function filmToHtml(film) {
+  return `<li data-id="${film.tmdb_id}" class="flex items-center gap-x-6 hover:bg-indigo-200 p-4" onclick="setFilmId(this)">
+            <img class="rounded-lg" src="https://image.tmdb.org/t/p/w92${film.poster_path}"></img>
+            <div class="grid">
+              <span class="font-semibold">${film.title}</span>
+              <span class="font-normal">${film.year}</span>
+            </div>
+          </li>`;
+}
+
+function setFilmId(film) {
+  tmdbId.value = film.dataset.id;
+
+  filmList.classList.add("hidden");
+  filmList.innerHTML = "";
+
+  filmSearchBar.classList.add("hidden");
+  filmSearchBar.value = "";
+
+  selectedFilm.classList.remove("hidden");
+  selectedFilm.innerHTML = film.innerHTML;
+}
+
+function removeFilmId() {
+  tmdbId.value = "";
+
+  filmList.classList.remove("hidden");
+  filmList.innerHTML = "";
+
+  filmSearchBar.classList.remove("hidden");
+  filmSearchBar.value = "";
+  filmSearchBar.focus();
+
+  selectedFilm.classList.add("hidden");
+  selectedFilm.innerHTML = "";
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.html.tera"],
+  content: ["./templates/*.html.tera", "./static/js/*.js"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
